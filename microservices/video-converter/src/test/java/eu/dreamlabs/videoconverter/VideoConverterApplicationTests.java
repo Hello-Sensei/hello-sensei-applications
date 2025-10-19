36d341d31412fd8325f84f408e51b19084eb5b29
davidvera13 <david.vera@9online.fr>
2025-10-19 17:46:38 +0200

    feat(video-converter): video converter startup project

---
+++ microservices/video-converter/src/test/java/eu/dreamlabs/videoconverter/VideoConverterApplicationTests.java
@@ -0,0 +1,13 @@
+package eu.dreamlabs.videoconverter;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VideoConverterApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
