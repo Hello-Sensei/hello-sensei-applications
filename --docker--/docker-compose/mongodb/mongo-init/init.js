201f58d58d541213440c386584b04bb4fbe6b71c
davidvera13 <david.vera@9online.fr>
2025-10-19 20:04:43 +0200

    feat(docker): updating mongo db

--- --docker--/docker-compose/mongodb/mongo-init/init.js
+++ --docker--/docker-compose/mongodb/mongo-init/init.js
@@ -1,3 +1,9 @@
+// first db for uploads
 db = db.getSiblingDB('videos_uploader');
 db.createCollection('videos_metadatas');
+
+// second db for converts
+db = db.getSiblingDB('videos_converter');
+db.createCollection('videos_metadatas');
+
 print('✅ videos_uploader database initialized');
